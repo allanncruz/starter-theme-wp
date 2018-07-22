fcdef7f2aa73c6cf971e13c300a86d09f2283717
Allan Dantas <allancruz716@gmail.com>
2018-07-22 11:14:46 -0300

    Implement gulp-concat
    Clear css

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,16 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+
+gulp.task('default',['sass','watch']);
 
 gulp.task('sass', function() {
-    return gulp.src(['node_modules/bootstrap/scss/*.scss', 'src/scss/*.scss'])
-    .pipe(sass()) // converter o Sass em CSS
-    .pipe(gulp.dest('src/css'));
+    return gulp.src(['node_modules/bootstrap/dist/css/bootstrap.css', 'src/scss/*.scss'])
+    .pipe(concat('style.min.css'))
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(gulp.dest('dist/css'));
 });
 
 gulp.task('watch', function() {
-    gulp.watch(['node_modules/bootstrap/scss/*.scss', 'src/scss/*.scss'], ['sass']);
+    gulp.watch(['src/scss/*.scss','src/scss/library/*.scss'], ['sass']);
 });
-
-gulp.task('default', ['sass', 'watch']);
