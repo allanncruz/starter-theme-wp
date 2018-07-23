29181ec7d249cdd65009bb8003dc9253741e52ea
Allan Dantas <allancruz716@gmail.com>
2018-07-23 20:11:08 -0300

    Add Gulp-copy fonts and imgages

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 
-gulp.task('default',['sass','js','watch']);
+gulp.task('default',['sass','js','copyfonts','copy-img','watch']);
 
 gulp.task('sass', function() {
     return gulp.src([
@@ -25,7 +25,18 @@ gulp.task('js', function() {
         .pipe(gulp.dest('dist/js'));
 });
 
+gulp.task('copyfonts', function() {
+    gulp.src('src/font/**/*')
+        .pipe(gulp.dest('dist/fonts'));
+});
+gulp.task('copy-img', function() {
+    gulp.src('src/img/**/*')
+        .pipe(gulp.dest('dist/img'));
+});
+
 gulp.task('watch', function() {
     gulp.watch('src/scss/**/*.scss', ['sass']);
     gulp.watch('src/js/*.js', ['js']);
+    gulp.watch('src/font/**/*', ['copyfonts']);
+    gulp.watch('src/img/**/*', ['copy-img']);
 });
