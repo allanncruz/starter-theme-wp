486e79d2c34b4c4d9a6dcfb4a61b48cf116ff2e5
Allan Dantas <allancruz716@gmail.com>
2018-07-28 10:45:02 -0300

    List services

--- index.php
+++ index.php
@@ -24,18 +24,78 @@
 
     <section class="company py-5">
         <div class="container">
-            <div class="row">
-                <div class="col-md-7">
-                    <?php $args = [ "post_type" => "page", "pagename" => "projeto" ];
-                    $projeto = new WP_Query($args);if($projeto->have_posts()):
-                        while($projeto->have_posts()): $projeto->the_post(); ?>
-                            <h1 class="title"><?php the_title(); ?></h1>
-                            <?php the_field('chamada'); ?>
-                            <a href="<?php bloginfo("url") ?>/projeto" class="btn btn-default">Saiba Mais</a>
-                        <?php endwhile; endif; ?>
+            <div class="box-container bg-white p-5 shadow-sm">
+                <div class="row">
+                    <div class="col-md-7">
+                        <?php $args = [ "post_type" => "page", "pagename" => "projeto" ];
+                        $projeto = new WP_Query($args);if($projeto->have_posts()):
+                            while($projeto->have_posts()): $projeto->the_post(); ?>
+                                <h1 class="title"><?php the_title(); ?></h1>
+                                <?php the_field('chamada'); ?>
+                                <a href="<?php bloginfo("url") ?>/projeto" class="btn btn-default">Saiba Mais</a>
+                            <?php endwhile; endif; ?>
+                    </div>
+                    <div class="col-md-5">
+                        <img class="photo-highlight w-100" src="<?php the_post_thumbnail_url(); ?>">
+                    </div>
                 </div>
-                <div class="col-md-5">
-                    <img class="photo-highlight w-100 mt-2" src="<?php the_post_thumbnail_url(); ?>">
+            </div>
+        </div>
+    </section>
+
+    <section class="services bg-white py-5">
+        <div class="container">
+
+            <h1 class="text-center">Services</h1>
+
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="card shadow-sm">
+                        <figure class="w-100 bg-default"
+                                style="background: url(<?php the_post_thumbnail_url(); ?>)"></figure>
+                        <div class="card-body">
+                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <div class="d-flex justify-content-between align-items-center">
+                                <div class="btn-group">
+                                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                                    <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                                </div>
+                                <small class="text-muted">9 mins</small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card shadow-sm">
+                        <figure class="w-100 bg-default"
+                                style="background: url(<?php the_post_thumbnail_url(); ?>)"></figure>
+                        <div class="card-body">
+                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <div class="d-flex justify-content-between align-items-center">
+                                <div class="btn-group">
+                                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                                    <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                                </div>
+                                <small class="text-muted">9 mins</small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card shadow-sm">
+                        <figure class="w-100 bg-default"
+                                style="background: url(<?php the_post_thumbnail_url(); ?>)"></figure>
+                        <div class="card-body">
+                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <div class="d-flex justify-content-between align-items-center">
+                                <div class="btn-group">
+                                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                                    <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                                </div>
+                                <small class="text-muted">9 mins</small>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
