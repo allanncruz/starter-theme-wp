c4b7ea7b5f403868bf18225ec0584d3dd59f92ac
Allan Dantas <allancruz716@gmail.com>
2018-08-03 00:06:53 -0300

    Add extensions to the general file scss
    Create single page

--- page.php
+++ page.php
@@ -3,14 +3,16 @@
     <section class="internal-page">
         <div class="container featurette mt-5">
 
-            <div class="row">
-                <div class="col-12 col-md-12">
-                    <?php if(have_posts()): while(have_posts()): the_post(); ?>
-                        <h1 class="title mb-4"><?php the_title(); ?></h1>
-                        <img onerror="this.style.display='none'" src="<?php the_post_thumbnail_url(); ?>"
-                             class="img-thumbnail photo-highlight w-100 p-0 shadow mt-2 mb-3 ml-4 rounded-0 float-right">
-                        <?php the_content(); ?>
-                    <?php endwhile; endif; ?>
+            <div class="box-container bg-white p-5 mb-5 shadow-sm">
+                <div class="row">
+                    <div class="col-12 col-md-12">
+                        <?php if(have_posts()): while(have_posts()): the_post(); ?>
+                            <h1 class="title mb-4"><?php the_title(); ?></h1>
+                            <img onerror="this.style.display='none'" src="<?php the_post_thumbnail_url(); ?>"
+                                 class="img-thumbnail photo-highlight w-100 p-0 shadow mt-2 mb-3 ml-4 rounded-0 float-right">
+                            <?php the_content(); ?>
+                        <?php endwhile; endif; ?>
+                    </div>
                 </div>
             </div>
         </div>
