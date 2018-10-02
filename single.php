a4d5aa676bd50b05eaa42880bfb2a5e5fa6a5e9b
Allan Dantas <allancruz716@gmail.com>
2018-10-02 10:02:35 -0300

    Float img-thumbnail and spacing

--- single.php
+++ single.php
@@ -7,11 +7,11 @@
                 <div class="col-12 col-md-12">
                     <?php if(have_posts()): while(have_posts()): the_post(); ?>
                         <h1 class="title title-default"><?php the_title(); ?></h1>
-                        <img onerror="this.style.display='none'" class="img-thumbnail foto-destaque"
+                        <img onerror="this.style.display='none'" class="img-thumbnail float-right foto-destaque"
                              src="<?php the_post_thumbnail_url(); ?>">
                         <?php the_content(); ?>
 
-                        <div class="a2a_kit a2a_kit_size_32 a2a_default_style" data-a2a-url="<?php the_permalink(); ?>" data-a2a-title="<?php the_title(); ?>">
+                        <div class="a2a_kit a2a_kit_size_32 a2a_default_style mt-4" data-a2a-url="<?php the_permalink(); ?>" data-a2a-title="<?php the_title(); ?>">
                             <a class="a2a_button_facebook"></a>
                             <a class="a2a_button_twitter"></a>
                             <a class="a2a_button_google_plus"></a>
