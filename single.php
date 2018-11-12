3ad0f58492cb21b33a450fffc3a550fcfa677ebc
Allan Dantas <allancruz716@gmail.com>
2018-11-12 16:11:31 -0300

    Fix img single thumbnail

--- single.php
+++ single.php
@@ -7,12 +7,12 @@
                 <div class="col-12 col-md-12">
                     <?php if(have_posts()): while(have_posts()): the_post(); ?>
                         <h1 class="title title-default"><?php the_title(); ?></h1>
-                        <img onerror="this.style.display='none'" class="single-thumbnail float-right mb-3 ml-3"
-                             src="<?php the_post_thumbnail_url(); ?>">
+                        <img onerror="this.style.display='none'" src="<?php the_post_thumbnail_url(); ?>"
+                             class="img-thumbnail photo-highlight w-100 p-0 shadow mt-2 mb-3 ml-4 rounded-0 float-right">
                         <?php the_content(); ?>
 
 
-                        <div class="row">
+                        <div class="row w-100">
                             <div class="col-md-6">
                                 <div class="a2a_kit a2a_kit_size_32 a2a_default_style mt-4" data-a2a-url="<?php the_permalink(); ?>" data-a2a-title="<?php the_title(); ?>">
                                     <a class="a2a_button_facebook"></a>
