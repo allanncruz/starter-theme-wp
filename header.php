7723ef34ad5cca5f38b70c30f7fa97d529e3f20a
allanncruz <contato@allancruz.com.br>
2019-08-31 15:05:44 -0300

    Update davbar

--- header.php
+++ header.php
@@ -9,21 +9,16 @@
 <body>
 
 <header class="header sticky-top">
-        <nav class="navbar navbar-expand-xl navbar-dark bg-dark" role="navigation">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container">
-
-            <a class="navbar-brand position-absolute" href="<?php bloginfo("url") ?>">
-                <img class="position-relative w-75" src="<?php bloginfo("template_url") ?>/dist/img/image/logo.png">
-            </a>
-
-            <button class="navbar-toggler text-right w-100 outline border-0"
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler border-0"
                     type="button"
                     data-toggle="collapse"
                     data-target="#bs-example-navbar-collapse-1"
                     aria-controls="bs-example-navbar-collapse-1"
                     aria-expanded="false"
                     aria-label="Toggle navigation">
-
                 <span class="navbar-toggler-icon"></span>
             </button>
             <?php
