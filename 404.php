0a0c36ed9a5af52fcafd5d5a8855ce5dd242d71f
allanncruz <contato@allancruz.com.br>
2019-10-25 00:26:14 -0300

    Update 404 page

--- 404.php
+++ 404.php
@@ -1,13 +1,11 @@
 <?php get_header(); ?>
 
-    <section class="internal-page">
-        <div class="container featurette mt-5">
-
-            <div class="box-container bg-white p-5 mb-5 shadow-sm">
+    <section class="wrap">
+        <div class="container content-area mt-5">
+            <div class="content-box bg-white p-md-5 p-3  shadow-sm">
                 <div class="row">
-                    <div class="col-12 col-md-12 text-center">
-                        <h1>Página não encontrada!</h1>
-
+                    <div class="col-12 col-md-12">
+                        <h1 class="display-4 text-center">Página não encontrada!</h1>
                         <p>O caminho para o conteúdo que você deseja aessar está errado ou a página podeter sido exluída</p>
                     </div>
                 </div>
