7d4fcac77459f4c68e15c555615d4fbbfc1c2171
allanncruz <contato@allancruz.com.br>
2019-12-19 21:40:13 -0300

    update wp attributes

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta <?php bloginfo('charset'); ?>>
 
-    <title><?php wp_title(); ?></title>
+    <title><?php bloginfo('title') ?></title>
     <?php wp_head(); ?>
 </head>
 <body class="bg-light">
