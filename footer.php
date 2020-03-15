03a376213f7a2101fdb65710f18c1994087edbc0
allanncruz <contato@allancruz.com.br>
2020-03-15 13:37:44 -0300

    echo content address and customize footer section

--- footer.php
+++ footer.php
@@ -1,17 +1,52 @@
-<footer class="footer">
+<footer class="footer text-white py-5">
     <div class="container">
-        <div class="row">
-            <div class="col-12 col-md-3">
-                <a class="footer--brand" href="<?php bloginfo("url") ?>">
-                    Boilerplate wp
+        <div class="row align-items-center">
+            <div class="col-md-2 p-0">
+                <a class="footer--brand text-center text-md-right" href="<?php bloginfo("url") ?>">
+                    <?php 
+                        $custom_logo_id = get_theme_mod('custom_logo');
+                        $logo = wp_get_attachment_image_src($custom_logo_id, 'full');
+
+
+                        if(has_custom_logo()) {
+                            echo '<img src="'. esc_url($logo[0]). '" class="img-fluid">';
+                        }else {
+                            echo '<p class="m-0 text-white">'. get_bloginfo('name'). '</p>';
+                        } 
+                    ?>
                 </a>
             </div>
 
-            <div class="col-md-6 text-center">
-                <a href="https://github.com/allanncruz/boilerplate-front-end-wp" class="m-0 h-100 text-white flex-item"
-                   target="_blank">
-                    Github</a>
-                <?php echo get_theme_mod('whatsapp_number') ?>
+            <div class="col-md-4 pl-5 pr-0 phone">
+                <div class="d-flex">
+                    <i class="fas fa-phone"></i>
+                    <?php echo get_theme_mod('phone_number') ?>
+                </div>
+                
+                <a href="https://api.whatsapp.com/send?phone=<?php echo get_theme_mod('whatsapp_number') ?>&text="
+                    class="d-flex d-md-none"
+                    target="_blank">
+                    <i class="fab fa-whatsapp"></i>
+                   <?php echo get_theme_mod('whatsapp_number') ?>
+                </a>
+                <a href="https://web.whatsapp.com/send?phone=<?php echo get_theme_mod('whatsapp_number') ?>&text="
+                    class="d-none d-md-flex"
+                    target="_blank">
+                    <i class="fab fa-whatsapp"></i>
+                    <?php echo get_theme_mod('whatsapp_number') ?>
+                </a>
+            </div>
+
+            <div class="col-md-4 pl-0 address">
+                <div class="d-flex">
+                    <i class="fas fa-map-marker-alt"></i>
+                    <?php echo get_theme_mod('address') ?>
+                </div>
+            </div>
+
+            <div class="col-md-2 social">
+                <a href="<?php echo get_theme_mod('instagram_username') ?>" target="_blank"><i class="fab fa-instagram ml-3"></i></a>
+                <a href="<?php echo get_theme_mod('facebook_username') ?>"  target="_blank"><i class="fab fa-facebook-square"></i></a>
             </div>
 
         </div>
