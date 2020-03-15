bf6bf1789a886c5cb49f202d5d14e08b180babdc
allanncruz <contato@allancruz.com.br>
2020-03-15 14:07:50 -0300

    update single pages

--- page.php
+++ page.php
@@ -1,13 +1,22 @@
 <?php get_header(); ?>
 
-    <section class="site-content">
-        <div class="container">
-            <?php if(have_posts()): while(have_posts()): the_post(); ?>
-                <h1 class="display-4 text-center"><?php the_title(); ?></h1>
-                <img onerror="this.style.display='none'" src="<?php the_post_thumbnail_url(); ?>"
-                        class="img-thumbnail photo-highlight w-100 p-0 shadow mt-2 mb-3 ml-4 rounded-0 float-right">
-                <?php the_content(); ?>
-            <?php endwhile; endif; ?>
+<?php the_post() ?>
+
+    <section class="wrap">
+        <div class="container content-area mt-5">
+            <div class="text-center">
+                <h3 class="sub-title m-0">Subtitle</h3>
+                <h1 class="title"><?php the_title(); ?></h1>
+            </div>  
+            
+            <div class="row justify-content-center">
+                <div class="col-md-10">
+                    <img onerror="this.style.display='none'" src="<?php the_post_thumbnail_url(); ?>" class="single-thumbnail my-4">
+                    <article class="px-md-5 px-2">
+                        <?php the_content(); ?>
+                    </article>
+                </div>
+            </div>
         </div>
     </section>
 
