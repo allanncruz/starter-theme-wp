e1cd4b390918bafa2ee7701e1ea82ff947fc4602
allanncruz <contato@allancruz.com.br>
2020-03-18 18:33:55 -0300

    define category exemple page

--- page-exemplos-categoria.php
+++ page-exemplos-categoria.php
@@ -14,6 +14,8 @@
                     <img onerror="this.style.display='none'" src="<?php the_post_thumbnail_url(); ?>" class="single-thumbnail my-4">
                     <article class="px-md-5 px-2">
                         <?php the_content(); ?>
+
+                        <h3 class="my-5">Lista de categorias seguido de postagens </h3>
                         <?php
                             // Get all the categories
                             $categories = get_terms( 'blog_taxonomy' );
@@ -37,12 +39,12 @@
                                 );
                             ?>
 
-                                <div class="list-carte">
-                                    <h3 class="list-carte__category text-center mb-3"><?php echo $category->name; ?></h3>
+                                <div class="category-carte">
+                                    <h3 class="category-carte__category text-center mb-3"><?php echo $category->name; ?></h3>
                                     
                                     
                                     <?php while ($cartes->have_posts()) : $cartes->the_post(); ?>
-                                        <div class="list-carte__post d-block d-md-flex text-center text-md-left mb-4">
+                                        <div class="category-carte__post d-block d-md-flex text-center text-md-left mb-4">
                                             <img class="post_thumbnail" onerror="this.style.display='none'" src="<?php the_post_thumbnail_url(); ?>" alt="<?php the_title(); ?>">
                                             <div class="content">
                                                 <h6 class="my-1"><?php the_title(); ?></h6>
