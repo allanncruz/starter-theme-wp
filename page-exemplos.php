21771c9a06ee19550328a9cf2c33a5d38b7f7d4e
allanncruz <contato@allancruz.com.br>
2020-03-17 23:50:50 -0300

    create posts by custom blog taxonomies

---
+++ page-exemplos.php
@@ -0,0 +1,72 @@
+<?php get_header(); ?>
+
+<?php the_post() ?>
+
+    <section class="wrap">
+        <div class="container content-area mt-5">
+            <div class="text-center">
+                <h3 class="sub-title m-0">Subtitle</h3>
+                <h1 class="title"><?php the_title(); ?></h1>
+            </div>  
+            
+            <div class="row justify-content-center">
+                <div class="col-md-10">
+                    <img onerror="this.style.display='none'" src="<?php the_post_thumbnail_url(); ?>" class="single-thumbnail my-4">
+                    <article class="px-md-5 px-2">
+                        <?php the_content(); ?>
+                        <?php
+                            // Get all the categories
+                            $categories = get_terms( 'blog_taxonomy' );
+
+                            // Loop through all the returned terms
+                            foreach ( $categories as $category ):
+
+                                // set up a new query for each category, pulling in related posts.
+                                $cartes = new WP_Query(
+                                    array(
+                                        'post_type' => 'blog',
+                                        'showposts' => -1,
+                                        'tax_query' => array(
+                                            array(
+                                                'taxonomy'  => 'blog_taxonomy',
+                                                'terms'     => array( $category->slug ),
+                                                'field'     => 'slug'
+                                            )
+                                        )
+                                    )
+                                );
+                            ?>
+
+                                <div class="list-carte">
+                                    <h3 class="list-carte__category text-center mb-3"><?php echo $category->name; ?></h3>
+                                    
+                                    
+                                    <?php while ($cartes->have_posts()) : $cartes->the_post(); ?>
+                                        <div class="list-carte__post d-block d-md-flex text-center text-md-left mb-4">
+                                            <img class="post_thumbnail" onerror="this.style.display='none'" src="<?php the_post_thumbnail_url(); ?>" alt="<?php the_title(); ?>">
+                                            <div class="content">
+                                                <h6 class="my-1"><?php the_title(); ?></h6>
+                                                <p class="m-1"><?php the_field('descricao'); ?></p>
+                                                <p class="price m-0"><?php the_field('preco'); ?></p>
+                                            </div>
+                                        </div>
+                                    <?php endwhile; ?>
+                                </div>
+
+                                <hr class="hr-carte">
+
+                            <?php
+                                // Reset things, for good measure
+                                $cartes = null;
+                                wp_reset_postdata();
+
+                            // end the loop
+                            endforeach;
+                            ?>
+                    </article>
+                </div>
+            </div>
+        </div>
+    </section>
+
+<?php get_footer(); ?>
