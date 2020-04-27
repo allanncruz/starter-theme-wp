0985f3ba869df09766fb1a67ff9b34cc26d2f675
allanncruz <contato@allancruz.com.br>
2020-04-26 22:25:11 -0300

    organize cards structure

--- partials/section-news.php
+++ partials/section-news.php
@@ -1,18 +1,25 @@
 
         <div class="<?php if ( is_page('blog')) echo "col-md-4"; ?>">
-            <a href="<?php the_permalink(); ?>">
-                <div class="cards-item mx-1">
-                    <div class="">
-                        <img class="cards-item__image" onerror="this.style.display='none'" src="<?php the_post_thumbnail_url(); ?>" />
-                    </div>
+
+                <div class="cards-item p-3 mx-1 bg-white">
+                    <a href="<?php the_permalink(); ?>">
+                        <img
+                            class="cards-item__image"
+                            onerror="this.style.display='none'"
+                            src="<?php the_post_thumbnail_url(); ?>"
+                        />
+                    </a>
                     <div class="cards-item__legend px-4 py-3">
                         <div class="cards-item__data text-white">
                             <?php the_time( get_option( 'date_format' ) ); ?>
                         </div>
+
                         <span class="card-category mt-3">
                             <?php echo get_the_term_list( get_the_ID(), 'blog_taxonomy', '#', ', ' ); ?>
                         </span>
-                        <h5 class="cards-item__title"><?php the_title(); ?></h5>
+                        <a href="<?php the_permalink(); ?>">
+                            <h5 class="cards-item__title"><?php the_title(); ?></h5>
+                        </a>
                     </div>
                 </div>
             </a>
