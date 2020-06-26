b8b2be7d5c8b77ddd7e07900970b84cded7c0e7c
Allan Cruz <42693739+allanncruz@users.noreply.github.com>
2020-06-26 18:09:01 -0300

    enable editor classic

--- functions.php
+++ functions.php
@@ -66,3 +66,6 @@ function custom_excerpt_length( $length ) {
 	return 20;
 }
 add_filter( 'excerpt_length', 'custom_excerpt_length', 999 );
+
+//remove editor gutenberg enable editor classic
+add_filter('use_block_editor_for_post', '__return_false');
