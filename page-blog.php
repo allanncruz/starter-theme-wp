162eb4abebbf0c7b814483dd24bf1e9458fc576c
allanncruz <contato@allancruz.com.br>
2020-06-29 08:56:37 -0300

    customize seach form

--- page-blog.php
+++ page-blog.php
@@ -5,59 +5,43 @@
     <section class="page">
         <div class="container page-area mt-5">
             <div class="text-center">
-                <h3 class="sub-title m-0">Subtitle</h3>
                 <h1 class="title"><?php the_title(); ?></h1>
+                <div class="page-seach w-50 m-auto">
+                    <form role="search" method="get" id="searchform" class="searchform" action="<?php echo home_url(); ?>">
+                        <div>
+                            <label class="screen-reader-text" for="s">Pesquisar por:</label>
+                            <input type="text" value="" placeholder="Pesquisar" name="s" id="s">
+                            <button type="submit" id="searchsubmit" class="border-0">
+                                <i class="fas fa-search"></i>
+                            </button>
+                        </div>
+                    </form>
+                </div>
             </div>  
             
-            <div class="row justify-content-center">
-                <div class="col-md-10">
-                    <img
-                        onerror="this.style.display='none'"
-                        src="<?php the_post_thumbnail_url(); ?>"
-                        class="single-thumbnail my-4"
-                        alt="<?php the_title(); ?>"
-                    >
-                    <article class="px-md-5 px-2">
-                        <?php the_content(); ?>
-                    </article>
-                    <div class="blog-seach">
-                        <form role="search" method="get" id="searchform" class="searchform" action="<?php echo home_url(); ?>">
-                            <div>
-                                <label class="screen-reader-text" for="s">Pesquisar por:</label>
-                                <input type="text" value="" placeholder="Pesquisar" name="s" id="s">
-                                <button type="submit" id="searchsubmit" class="border-0">
-                                    <i class="fas fa-search"></i>
-                                </button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-                <div class="col-md-12">
-                    <div class="row">
-                        <?php
-                            $the_query = new WP_Query( array('posts_per_page'=>2,
-                                'post_type'=>'blog',
-                                'paged' => get_query_var('paged') ? get_query_var('paged') : 1) );  
-                                while ($the_query -> have_posts()) : $the_query -> the_post(); 
-                                    get_template_part( 'partials/section', 'news' ); 
-                                    
-                                endwhile;  
-                        ?>
-                    </div>
-                        <div class="text-center mt-5">
-                            <?php
-                                $big = 999999999; // need an unlikely integer
-                                echo paginate_links( array(
-                                'base' => str_replace( $big, '%#%', get_pagenum_link( $big ) ),
-                                'format' => '?paged=%#%',
-                                'current' => max( 1, get_query_var('paged') ),
-                                'total' => $the_query->max_num_pages
-                                ) );
+            <div class="row">
+                <?php
+                    $the_query = new WP_Query( array('posts_per_page'=>2,
+                        'post_type'=>'blog',
+                        'paged' => get_query_var('paged') ? get_query_var('paged') : 1) );  
+                        while ($the_query -> have_posts()) : $the_query -> the_post(); 
+                            get_template_part( 'partials/section', 'news' ); 
+                            
+                        endwhile;  
+                ?>
+            </div>
+            <div class="text-center mt-5">
+                <?php
+                    $big = 999999999; // need an unlikely integer
+                    echo paginate_links( array(
+                    'base' => str_replace( $big, '%#%', get_pagenum_link( $big ) ),
+                    'format' => '?paged=%#%',
+                    'current' => max( 1, get_query_var('paged') ),
+                    'total' => $the_query->max_num_pages
+                    ) );
 
-                                wp_reset_postdata();
-                            ?>
-                        </div>
-                </div>
+                    wp_reset_postdata();
+                ?>
             </div>
         </div>
     </section>
--- search.php
+++ search.php
@@ -8,18 +8,18 @@ Template Name: Search Page
     <section class="page">
         <div class="container page-area mt-5">
             <div class="text-center">
-                <h3 class="sub-title m-0">Resultado da busca para: <b><?php echo isset($_GET['s']) ? $_GET['s'] : ''; ?></h3>
-            </div>  
-            <div class="blog-seach">
-                <form role="search" method="get" id="searchform" class="searchform" action="<?php echo home_url(); ?>">
-                    <div>
-                        <label class="screen-reader-text" for="s">Pesquisar por:</label>
-                        <input type="text" value="" placeholder="Pesquisar" name="s" id="s">
-                        <button type="submit" id="searchsubmit" class="border-0">
-                            <i class="fas fa-search"></i>
-                        </button>
-                    </div>
-                </form>
+                <h3 class="sub-title">Resultado da busca para: <b><?php echo isset($_GET['s']) ? $_GET['s'] : ''; ?></h3>
+                <div class="page-seach w-50 m-auto">
+                    <form role="search" method="get" id="searchform" class="searchform" action="<?php echo home_url(); ?>">
+                        <div>
+                            <label class="screen-reader-text" for="s">Pesquisar por:</label>
+                            <input type="text" value="" placeholder="Pesquisar" name="s" id="s">
+                            <button type="submit" id="searchsubmit" class="border-0">
+                                <i class="fas fa-search"></i>
+                            </button>
+                        </div>
+                    </form>
+                </div>
             </div>
             
             <div class="row">
