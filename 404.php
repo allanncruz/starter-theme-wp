d0b4a0053466fc5667b15109a0ff082cd9129908
allanncruz <contato@allancruz.com.br>
2020-08-22 21:09:25 -0300

    add coment template for displaying

--- 404.php
+++ 404.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * The template for displaying 404 pages (not found)
+ *
+ * @link https://codex.wordpress.org/Creating_an_Error_404_Page
+ *
+ * @package Started theme wp
+ */
     get_header();
     the_post() ?>
 
--- search.php
+++ search.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * The template for displaying search results pages
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#search-result
+ *
+ * @package Started theme wp
+ */
+
 /*
 Template Name: Search Page
 */
