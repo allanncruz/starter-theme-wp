13c4b08a210470ede8c75647e7d617e2d63a347e
allanncruz <contato@allancruz.com.br>
2021-02-18 23:45:25 -0300

    disable the emoji's

--- functions.php
+++ functions.php
@@ -55,7 +55,7 @@ require_once get_template_directory() . '/class-wp-bootstrap-navwalker.php';
 
     register_nav_menus( array(
        'principal' => __('Menu Principal', 'bstwp')
-    ));    
+    ));
 
     
 
@@ -84,3 +84,31 @@ add_filter( 'excerpt_length', 'custom_excerpt_length', 999 );
 
 //remove editor gutenberg enable editor classic
 add_filter('use_block_editor_for_post', '__return_false');
+  
+  /**
+   * Disable the emoji's
+   */
+  function disable_emojis() {
+    remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+    remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+    remove_action( 'wp_print_styles', 'print_emoji_styles' );
+    remove_action( 'admin_print_styles', 'print_emoji_styles' );
+    remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+    remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
+    remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+    
+    // Remove from TinyMCE
+    add_filter( 'tiny_mce_plugins', 'disable_emojis_tinymce' );
+  }
+  add_action( 'init', 'disable_emojis' );
+  
+  /**
+   * Filter out the tinymce emoji plugin.
+   */
+  function disable_emojis_tinymce( $plugins ) {
+    if ( is_array( $plugins ) ) {
+      return array_diff( $plugins, array( 'wpemoji' ) );
+    } else {
+      return array();
+    }
+  }
