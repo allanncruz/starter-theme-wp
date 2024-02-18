aeb63c05f05e6c0c3e74a6ef03fcc0a17bb976a0
Allan <contato@allancruz.com.br>
2024-02-17 21:36:42 -0300

    feat: remove editor gutenberg

--- functions.php
+++ functions.php
@@ -5,13 +5,16 @@
  *
  * @link https://developer.wordpress.org/themes/basics/theme-functions/
  *
- * @package _s
+ * @package Started theme wp
  */
 
 if (!defined('_S_VERSION')) {
 	// Replace the version number of the theme on each release.
 	define('_S_VERSION', '1.0.0');
 }
+
+//remove editor gutenberg enable editor classic
+add_filter('use_block_editor_for_post', '__return_false');
 
 /**
  * Sets up theme defaults and registers support for various WordPress features.
